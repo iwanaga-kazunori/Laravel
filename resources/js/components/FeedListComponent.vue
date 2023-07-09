2357667a672734020bdd82f2fe5c7c59cf66daa9
iwanaga-kazunori <93032151+iwanaga-kazunori@users.noreply.github.com>
2023-07-09 16:04:43 +0900

    Add files via upload

--- resources/js/components/FeedListComponent.vue
+++ resources/js/components/FeedListComponent.vue
@@ -126,7 +126,7 @@ export default {
         // ページネーション時の新着情報取得
         getFeeds (store, page) {
             let token = document.head.querySelector('meta[name=api-token]')
-            const url = '/api/feed?page=' + page + '&api_token=' + token.content         
+            const url = '/laravel/api/feed?page=' + page + '&api_token=' + token.content         
             axios.get(url)
                 .then(function (response) {
                     store.commit('setFeeds', response.data.data)
